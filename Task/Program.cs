0c775a85e45daf71e705c4f13df10fdcc5c0a5f0
Diana <Ladidi997@mail.ru>
2022-09-19 21:17:49 +0300

    Инициализировали массивы

---
+++ Task/Program.cs
@@ -0,0 +1,5 @@
+﻿// See https://aka.ms/new-console-template for more information
+Console.Clear();
+
+string[] FirstArray = new string[7] {"Friend", "Git", "7856", "12", "Close", "Test", "Cow"};
+string[] SecondArray = new string[FirstArray.Length];
